3dd0749b73b6ab1a73677ba756283f16b8d3d7c6
PhamThi1710 <lsjenny1710@gmail.com>
2023-12-09 21:47:51 +0700

    update btnChangeInfo

--- Login_with_Gmail/infoUser.php
+++ Login_with_Gmail/infoUser.php
@@ -1,11 +1,12 @@
 <?php
-    session_start();
+session_start();
 
-    @include 'database.php';
+@include 'database.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,10 +14,11 @@
     <title>Dịch vụ sinh viên</title>
 
     <!-- custom css file link -->
-    <link rel="stylesheet" type="text/css" href="../style.css" >
-    <link rel="stylesheet" type="text/css" href="../BuyPrintingPages/BuyPrintingPages.css" >
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="../BuyPrintingPages/BuyPrintingPages.css">
 
 </head>
+
 <body>
     <!-- header section starts -->
 
@@ -31,22 +33,22 @@
 
             <div class="menu-bar">
                 <div class="first-option"><a href="../UserHome/BeforeLoad.php">trang chủ</a></div>
-                <div class="second-option"><a href="homeAfterLogin_User.php" >dịch vụ của tôi</a></div>
+                <div class="second-option"><a href="homeAfterLogin_User.php">dịch vụ của tôi</a></div>
             </div>
         </div>
-        
+
         <div class="right-side">
             <div class="username"><a href="infoUser.php">
-                <?php
+                    <?php
                     if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['name'])) {
                         echo htmlspecialchars($_SESSION['user_info']['name']);
                     }
-                ?>
+                    ?>
                 </a>
             </div>
 
             <div class="seperator">|</div>
-            
+
             <div>
                 <a href="home.php" class="logout">Đăng xuất</a>
             </div>
@@ -63,61 +65,70 @@
     <div class="body">
         <h1 class="title">
             <?php
-                if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['name'])) {
-                    echo '<span class="user-name">' . htmlspecialchars($_SESSION['user_info']['name']) . '</span>';
-                }
+            if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['name'])) {
+                echo '<span class="user-name">' . htmlspecialchars($_SESSION['user_info']['name']) . '</span>';
+            }
             ?>
         </h1>
 
         <div class="service-list">
-            <div><h2>
-                Họ và tên:
-                <?php
-                if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['name'])) {
-                    echo '<span class="user-name">' . htmlspecialchars($_SESSION['user_info']['name']) . '</span>';
-                }
-                ?>
-            </h2></div>
+            <div>
+                <h2>
+                    Họ và tên:
+                    <?php
+                    if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['name'])) {
+                        echo '<span class="user-name">' . htmlspecialchars($_SESSION['user_info']['name']) . '</span>';
+                    }
+                    ?>
+                </h2>
+            </div>
 
-            <div><h2>
-                Email:
-                <?php
-                if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['email'])) {
-                    echo '<span class="user-name">' . htmlspecialchars($_SESSION['user_info']['email']) . '</span>';
-                }
-                ?>
-            </h2></div>
+            <div>
+                <h2>
+                    Email:
+                    <?php
+                    if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info']['email'])) {
+                        echo '<span class="user-name">' . htmlspecialchars($_SESSION['user_info']['email']) . '</span>';
+                    }
+                    ?>
+                </h2>
+            </div>
 
-            <div><h2>
-                Năm sinh:
-                <?php
-                $email = $_SESSION['user_info']['email'];
-                $get = mysqli_query($conn, "select Date_Of_Birth from users where email = '$email' ");
-                $getData = $get->fetch_all(MYSQLI_ASSOC);
-                $Date_Of_Birth = $getData[0]['Date_Of_Birth'];
-                echo '<span class="user-name">' . htmlspecialchars($Date_Of_Birth) . '</span>';
-                ?>
-            </h2></div>
+            <div>
+                <h2>
+                    Năm sinh:
+                    <?php
+                    $email = $_SESSION['user_info']['email'];
+                    $get = mysqli_query($conn, "select Date_Of_Birth from users where email = '$email' ");
+                    $getData = $get->fetch_all(MYSQLI_ASSOC);
+                    $Date_Of_Birth = $getData[0]['Date_Of_Birth'];
+                    echo '<span class="user-name">' . htmlspecialchars($Date_Of_Birth) . '</span>';
+                    ?>
+                </h2>
+            </div>
 
-            <div><h2>
-                Giới tính:
-                <?php
-                $email = $_SESSION['user_info']['email'];
-                $get = mysqli_query($conn, "select Sex from users where email = '$email' ");
-                $getData = $get->fetch_all(MYSQLI_ASSOC);
-                $Sex = $getData[0]['Sex'];
-                if($Sex == '0'){
-                    $x = "Nữ";
-                    echo '<span class="user-name">' . htmlspecialchars($x) . '</span>';
-                }else if($Sex == '1'){
-                    $x = "Nam";
-                    echo '<span class="user-name">' . htmlspecialchars($x) . '</span>';
-                }
-                ?>
-            </h2></div>
-
-            <form method ="POST" action="insertNewInfo.php" class="registration">
-                <button type="submit" class="submit-order" name="Change">Thay đổi thông tin</button>
+            <div>
+                <h2>
+                    Giới tính:
+                    <?php
+                    $email = $_SESSION['user_info']['email'];
+                    $get = mysqli_query($conn, "select Sex from users where email = '$email' ");
+                    $getData = $get->fetch_all(MYSQLI_ASSOC);
+                    $Sex = $getData[0]['Sex'];
+                    if ($Sex == '0') {
+                        $x = "Nữ";
+                        echo '<span class="user-name">' . htmlspecialchars($x) . '</span>';
+                    } else if ($Sex == '1') {
+                        $x = "Nam";
+                        echo '<span class="user-name">' . htmlspecialchars($x) . '</span>';
+                    }
+                    ?>
+                </h2>
+            </div>
+            <form method="" action="" class="registration">
+                <button type="button" class="submit-order" onClick="document.location.href='insertNewInfo.php'">Thay đổi
+                    thông
+                    tin</button>
             </form>
 
         </div>
@@ -147,9 +158,16 @@
 
                 <div class="box">
                     <h3>liên hệ</h3>
-                    <a href="#"> <div class="location-icon"></div>268 Ly Thuong Kiet Street Ward 14, District 10, Ho Chi Minh City, Vietnam </a>
-                    <a href="#"> <div class="phone-icon"></div>(028) 38 651 670 - (028) 38 647 256 (Ext: 5258, 5234) </a>
-                    <a href="mailto:elearning@hcmut.edu.vn" class="email"> <div class="email-icon"></div>elearning@hcmut.edu.vn </a>
+                    <a href="#">
+                        <div class="location-icon"></div>268 Ly Thuong Kiet Street Ward 14, District 10, Ho Chi Minh
+                        City, Vietnam
+                    </a>
+                    <a href="#">
+                        <div class="phone-icon"></div>(028) 38 651 670 - (028) 38 647 256 (Ext: 5258, 5234)
+                    </a>
+                    <a href="mailto:elearning@hcmut.edu.vn" class="email">
+                        <div class="email-icon"></div>elearning@hcmut.edu.vn
+                    </a>
                 </div>
             </div>
         </section>
@@ -172,11 +190,12 @@
 
     <!-- custom js file link -->
 </body>
+
 </html>
 
 <script>
     localStorage.setItem("ID", <?php echo $_SESSION['student'] ?>);
     localStorage.setItem("Role", <?php echo $_SESSION['role'] ?>);
-    localStorage.setItem("Username",<?php echo "\"". $_SESSION["name"] ."\"" ?>);
+    localStorage.setItem("Username", <?php echo "\"" . $_SESSION["name"] . "\"" ?>);
 
 </script>
