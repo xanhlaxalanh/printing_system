ecc78e6860bde768bdf0d2c97acfb6fcef83fdcd
Athena <bqh1402003@gmail.com>
2023-12-09 14:44:19 +0700

    add printer: fix add room to printer

--- ManagePrinters/addPrinterToDB.php
+++ ManagePrinters/addPrinterToDB.php
@@ -7,11 +7,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $printerDesc = $_POST['printerDesc'];
     $campus = $_POST['campus'];
     $building = $_POST['building'];
+    $room = $_POST['printerRoom'];
 
     //default setting when add is printer turn OFF
-    $query = "INSERT INTO printer_list (printer_id, printer_name, printer_desc, printer_avai, printer_campusloc, printer_buildingloc) VALUES (?, ?, ?, 'N', ?, ?)";
+    $query = "INSERT INTO printer_list (printer_id, printer_name, printer_desc, printer_avai, printer_campusloc, printer_buildingloc, printer_room) VALUES (?, ?, ?, 'N', ?, ?, ?)";
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("sssss", $printerId, $printerName, $printerDesc, $campus, $building);
+    $stmt->bind_param("ssssss", $printerId, $printerName, $printerDesc, $campus, $building, $room);
 
     if ($stmt->execute()) {
         echo "<script>alert('New printer added successfully'); window.location = 'addPrinter.php'</script>";
